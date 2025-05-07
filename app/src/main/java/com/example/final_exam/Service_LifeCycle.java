253ee59b1a5292dc1eb0e9f560111f68ff411d3c
Parth-12pm <parthsmahadik12027@gmail.com>
2025-05-07 21:10:19 +0530

    feat: Implement Service and Update Manifest
    This commit introduces a new service (`Service_LifeCycle`) and updates the AndroidManifest to include it.
    
    -   **Service_LifeCycle**: Added `Service_LifeCycle` as a new service class with `onBind` method.
    -   **AndroidManifest**: Updated `AndroidManifest.xml` to include the declaration of the `Service_LifeCycle`.
    - **AndroidManifest**: changed the launcher activity to `Animations` .

---
+++ app/src/main/java/com/example/final_exam/Service_LifeCycle.java
@@ -0,0 +1,16 @@
+package com.example.final_exam;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Service_LifeCycle extends Service {
+    public Service_LifeCycle() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
