9b51e2cf7aa75e2b347117145ef651a94fcebc36
Parth-12pm <parthsmahadik12027@gmail.com>
2025-05-08 23:42:30 +0530

    feat: Implement Service Lifecycle with Media Player
    This commit introduces a service (`Service_LifeCycle`) that manages media playback and demonstrates the service lifecycle methods.
    
    -   **Service_LifeCycle**: Added `Service_LifeCycle` to handle service lifecycle events.
    -   **onCreate**: Implemented `onCreate` to display a "Service Created" toast message.
    -   **onStartCommand**: Added `onStartCommand` to start playing `music1.mp3` when the service starts.
    -   **onDestroy**: Implemented `onDestroy` to pause the media player when the service is destroyed.
    -   **MediaPlayer**: Added a `MediaPlayer` instance (`m1`) to play audio.
    - **R.raw.music1**: added `music1.mp3` in raw directory.
    - **onBind**: added `return null;`.

--- app/src/main/java/com/example/final_exam/Service_LifeCycle.java
+++ app/src/main/java/com/example/final_exam/Service_LifeCycle.java
@@ -2,7 +2,9 @@ package com.example.final_exam;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class Service_LifeCycle extends Service {
 
@@ -10,9 +12,30 @@ public class Service_LifeCycle extends Service {
     public Service_LifeCycle() {
     }
 
+    MediaPlayer m1;
+
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(getApplicationContext(), "Service Created", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        m1 = MediaPlayer.create(this, R.raw.music1);
+        m1.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        m1.pause();
     }
 }
